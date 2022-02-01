ffd87682eca1122c18af452cbbcff208e87eef18
sofia-covarrubias <sofiacovarrubias1987@gmail.com>
2022-02-01 08:49:06 -0800

    Basic Application Java Class has aspects of application.

---
+++ src/Application.java
@@ -0,0 +1,91 @@
+public class Application
+{
+    // instance variables
+    private String name;
+    private String email;
+    private String phoneNum;
+    private String address;
+    private String hireDate;
+    private String federalEIN;
+    private String socialSecurity;
+
+    // constructor
+    public Application (String name, String email, String phoneNum, String address, String hireDate, String federalEIN, String socialSecurity)
+    {
+        this.name = name;
+        this.email = email;
+        this.phoneNum = phoneNum;
+        this.address = address;
+        this.hireDate = hireDate;
+        this.federalEIN = federalEIN;
+        this.socialSecurity = socialSecurity;
+    }
+
+    // getters
+    public String getName ()
+    {
+        return name;
+    }
+    public String getEmail ()
+    {
+        return email;
+    }
+    public String getPhoneNum ()
+    {
+        return phoneNum;
+    }
+    public String getAddress ()
+    {
+        return address;
+    }
+    public String getHireDate ()
+    {
+        return hireDate;
+    }
+    public String getFederalEIN ()
+    {
+        return federalEIN;
+    }
+    public String getSocialSecurity ()
+    {
+        return socialSecurity;
+    }
+
+    // setters
+    public void setName (String name)
+    {
+        this.name = name;
+    }
+    public void setEmail (String email)
+    {
+        this.email = email;
+    }
+    public void setPhoneNum (String phoneNum)
+    {
+        this.phoneNum = phoneNum;
+    }
+    public void setAddress (String address)
+    {
+        this.address = address;
+    }
+    public void setHireDate (String hireDate)
+    {
+        this.hireDate = hireDate;
+    }
+    public void setFederalEIN (String federalEIN)
+    {
+        this.federalEIN = federalEIN;
+    }
+    public void setSocialSecurity (String socialSecurity)
+    {
+        this.socialSecurity = socialSecurity;
+    }
+
+    // methods
+    public String toString ()
+    {
+        String output = "Name: " + name + "\nEmail: " + email + "\nPhone Number: " + phoneNum
+        + "\nAddress: " + address + "\nHire Date: " + hireDate + "\nEIN: " + federalEIN + "\nSSN: " + socialSecurity;
+        return output;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main
+{
+    public static void main (String[] args)
+    {
+        Application bro = new Application ("Purple Guy", "sofia@gmail.com", "9511234567", "123 bro st", "01252004", "123455555", "123456789");
+        System.out.println(bro.toString());
+    }
+}
