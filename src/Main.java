1a1aa7bbb96e054a9c0de005ac35249ca283d437
sofia-covarrubias <sofiacovarrubias1987@gmail.com>
2022-02-07 08:27:49 -0800

    FIXED FULL NAME VALIDATION.

--- src/Main.java
+++ src/Main.java
@@ -10,11 +10,14 @@ public class Main
         Application application = new Application();
         Scanner scan = new Scanner(System.in);
 
+        // Night 1 Phone Guy lines
+        System.out.println("HELLO! IF YOU ARE HERE, YOU ARE APPLYING FOR A JOB AT FREDDY FAZBEAR\'S PIZZA AS A NIGHT GUARD. FILL OUT THE FOLLOWING INFORMATION. \nUh, let's see, first there's an introductory greeting from the company that I'm supposed to read. Uh, it's kind of a legal thing, you know. Um, Welcome to Freddy Fazbear's Pizza. \nA magical place for kids and grown-ups alike, where fantasy and fun come to life. Fazbear Entertainment is not responsible for damage to property or person. \nUpon discovering that damage or death has occurred, a missing person report will be filed within 90 days, or as soon property and premises have been thoroughly \ncleaned and bleached, and the carpets have been replaced.");
+
         // getting initial input of name
-        System.out.println("Enter full name: ");
+        System.out.println("\nEnter full name: ");
         name = scan.nextLine();
         // checking is name is all letters
-        while (!name.matches("[A-Za-z]+"))
+        while (!name.matches("^[\\p{L} .'-]+$"))
         {
             System.out.println("Must be all letters. Enter full name: ");
             name = scan.nextLine();
@@ -24,7 +27,7 @@ public class Main
         System.out.println(application.toString());
 
         // getting initial input of email
-        System.out.println("Enter email address: ");
+        System.out.println("\nEnter email address: ");
         email = scan.nextLine();
         // email must have @ and then .com
         while (!email.matches("^.+@.+\\.(com|net)"))
@@ -37,7 +40,7 @@ public class Main
         System.out.println(application.toString());
 
         // getting initial phone number input
-        System.out.println("Enter phone number: ");
+        System.out.println("\nEnter phone number: ");
         phoneNum = scan.nextLine();
         while (!phoneNum.matches("\\([0-9]{3}\\)[0-9]{3}-[0-9]{4}"))
         {
@@ -49,7 +52,7 @@ public class Main
         System.out.println(application.toString());
 
         // getting initial federal EIN numbers
-        System.out.println("Enter federal EIN: ");
+        System.out.println("\nEnter federal EIN: ");
         federalEIN = scan.nextLine();
         while (!federalEIN.matches("[0-9]{2}-[0-9]{7}"))
         {
@@ -61,7 +64,7 @@ public class Main
         System.out.println(application.toString());
 
         // getting initial Social Security Number
-        System.out.println("Enter SSN: ");
+        System.out.println("\nEnter SSN: ");
         socialSecurity = scan.nextLine();
         while (!socialSecurity.matches("[0-9]{3}-[0-9]{2}-[0-9]{4}"))
         {
@@ -75,7 +78,7 @@ public class Main
 
 
         // getting initial street address
-        System.out.println("Enter your street address: ");
+        System.out.println("\nEnter your street address: ");
         streetAddress = scan.nextLine();
         while (!streetAddress.matches("[0-9]{1,5} [A-Za-z]+ (st|ave|dr|cir|blvd)"))
         {
@@ -83,7 +86,7 @@ public class Main
             streetAddress = scan.nextLine();
         } // end while
         // getting second portion of address
-        System.out.println("Enter your city, state, and zipcode: ");
+        System.out.println("\nEnter your city, state, and zipcode: ");
         secondAddress = scan.nextLine();
         while (!secondAddress.matches("[A-Za-z]+, [A-Z]{2} [0-9]{5}"))
         {
@@ -94,6 +97,7 @@ public class Main
         application.setAddress(streetAddress + ", " + secondAddress);
 
         System.out.println(application.toString());
+
 
     } // end main method
 } // end Main class
